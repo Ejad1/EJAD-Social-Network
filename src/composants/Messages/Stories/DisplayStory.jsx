ebcd0c23188ef62787528ace1e109a744fbce7d1
Ejad1 <edoumeyan@gmail.com>
2023-11-10 03:32:36 +0000

    Replacement of the story response box

--- src/composants/Messages/Stories/DisplayStory.jsx
+++ src/composants/Messages/Stories/DisplayStory.jsx
@@ -2,31 +2,43 @@ import { Avatar, Box, Card, CardActions, CardContent, CardHeader, CardMedia, Ico
 import { InsertEmoticon, Send } from '@mui/icons-material'
 import AttachFileIcon from '@mui/icons-material/AttachFile';
 import KeyboardVoiceIcon from '@mui/icons-material/KeyboardVoice';
-import DeleteIcon from '@mui/icons-material/Delete';
+import CloseIcon from '@mui/icons-material/Close';
+import storyVideo from '../../../assets/Dan Romer - Ending Theme [THE GOOD DOCTOR SOUNDTRACK].mp4'
+import storyImage from '../../../assets/story.jpg'
 
 export default function DisplayStory() {
   return (
     <div>
-        <Card sx={{ position: 'fixed', left: '30%', right: '1%' }}>
+        <Card sx={{ position: 'relative', left: '7%', overflowY: 'auto' }}>
             <CardHeader
                 avatar= { <Avatar>EJAD</Avatar> }
                 action= {
                     <IconButton aria-label="delete">
-                        <DeleteIcon sx={{ color: 'red' }}></DeleteIcon>
+                        <CloseIcon sx={{ color: 'red' }}></CloseIcon>
                     </IconButton>
                 }
                 title="EJAD"
                 subheader="November 07, 2023"
-            ></CardHeader>
+            ></CardHeader>            
+
+            <CardMedia
+                component="img"
+                height="480px"
+                image={ storyImage }
+                alt="Story"
+            ></CardMedia>
 
             <CardContent>Contenu de la story</CardContent>
 
             <CardMedia
-                component="img"
-                height="auto"
-                // image={ pubImageSource }
-                alt="Publication Image"
+                component="video"
+                height="500px"
+                controls
+                src={ storyVideo }
+                alt="Story"
             ></CardMedia>
+
+<CardContent>Contenu de la story</CardContent>
 
             <CardActions disableSpacing>
                 <Box sx={{ 
@@ -34,23 +46,23 @@ export default function DisplayStory() {
                     bottom: '5px',
                     paddingBottom: '5px',
                     paddingLeft: '2px',
-                    paddingRight: '6px',
-                    marginLeft: '-2px',
+                    paddingRight: '4px',
+                    marginLeft: '-1.2%',
                     width: '68%'
                 }}>
                     <input type="file" name="" id="file" style={{ display: 'none' }}/>
 
                     <IconButton>
                         <AttachFileIcon sx={{
-                            width: '35px', 
-                            height: '35px',
+                            width: '30px', 
+                            height: '30px',
                         }}/>
                     </IconButton>
 
                     <IconButton>
                         <InsertEmoticon sx={{
-                            width: '40px', 
-                            height: '40px',
+                            width: '35px', 
+                            height: '35px',
                         }}
                         ></InsertEmoticon>
                     </IconButton>
@@ -59,20 +71,20 @@ export default function DisplayStory() {
                         multiline maxRows={2} 
                         className='textField'
                         placeholder="Your message"
-                        sx={{ width: '80%' }}
+                        sx={{ width: '70%' }}
                     ></TextField>
 
                     <IconButton>
                         <KeyboardVoiceIcon sx={{
-                            width: '35px', 
-                            height: '35px',
+                            width: '30px', 
+                            height: '30px',
                         }}/>
                     </IconButton>
 
                     <IconButton>
                         <Send sx={{
-                            width: '40px', 
-                            height: '40px',
+                            width: '35px', 
+                            height: '35px',
                             marginRight: '-15px'
                         }}></Send>
                     </IconButton>
