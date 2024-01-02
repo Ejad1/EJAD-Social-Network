863e109cd572f033f3eafacfa77f5e998340f8dc
Ejad1 <edoumeyan@gmail.com>
2024-01-02 11:54:16 +0000

    Fixing the audio record bug succesfully (finnaly)

--- src/composants/Messages/AConversation/UserAnswer/UserAnswer.jsx
+++ src/composants/Messages/AConversation/UserAnswer/UserAnswer.jsx
@@ -1,5 +1,8 @@
 import { PropTypes } from "prop-types";
+import { useState, useRef } from 'react';
 import '../../../../css/Messages/UserAnswer.css'
+
+let myAudioBlob = false;
 
 export function UserAnswer({ answer, answerTime }) {
 
@@ -16,39 +19,89 @@ export function UserAnswer({ answer, answerTime }) {
         )
     }
     else if (answer.type === "audio") {
-        console.log("My answer in the audio block is : " + answer);
-
-        if (answer.data instanceof Blob) {
-            console.log("Blob data is valid");
-        } else {
-            console.error("Invalid Blob data");
-        }
 
         return (
-            <div className="user-answer">
-                <p>The audio will be here</p>
-                <p>Between me and </p>
-                <audio controls>
-                    <source src={ answer.data }></source>
-                    {/* { answer.data } */}
-                </audio>
-                <p>Me</p>
-                <h5 id="answer-date">{ answerTime }</h5>
+            <div className="user-answer audio-part">
+                {myAudioBlob && (
+                    <audio controls style={{ marginLeft: '67%' }}>
+                        <source src={URL.createObjectURL(myAudioBlob)} type="audio/wav" />
+                    </audio>
+                )}
             </div>
         )
     }
     else {
         return (
-            <>
-                <audio controls>
-                    <source src={ URL.createObjectURL(answer.data) } type="audio/wav" />
-                </audio>
-            </>
+           <h2>The message(s) type is not supported</h2>
         )
     }
 }
 
+
+
+
+export function AudioRecorder({ sendAudio, nomConv }) {
+    const [isRecording, setIsRecording] = useState(true);
+    const mediaRecorder = useRef(null);
+
+    // Start recording
+    navigator.mediaDevices.getUserMedia({ audio: true, })
+    .then((stream) => {
+        mediaRecorder.current = new MediaRecorder(stream);
+        const chunks = [];
+
+        mediaRecorder.current.ondataavailable = (event) => {
+        if (event.data.size > 0) {
+            chunks.push(event.data);
+        }
+        };
+
+        mediaRecorder.current.onstop = () => {
+            if (mediaRecorder.current && mediaRecorder.current.mimeType) {
+                const blob = new Blob(chunks, { type: mediaRecorder.current.mimeType });
+                myAudioBlob = blob;
+            }
+        };
+
+        mediaRecorder.current.start();
+        setIsRecording(true);
+    })
+    .catch((error) => console.error('Erreur lors de la demande d\'autorisation pour l\'audio :', error));
+
+
+    const stopRecording = () => {
+        if (mediaRecorder.current && mediaRecorder.current.state === 'recording') {
+            mediaRecorder.current.stop();
+            setIsRecording(false);
+
+            const myAudio = {
+                nom: nomConv,
+                type: "audio",
+                data: myAudioBlob
+            };
+
+            sendAudio(myAudio);
+        }
+    };
+
+    return (
+        <div>
+        <button onClick={stopRecording} disabled={!isRecording}>
+            Arrêter enregistrement
+        </button>
+        </div>
+    );
+}
+
+
+
 UserAnswer.propTypes = {
     answer: PropTypes.object.isRequired,
     answerTime: PropTypes.string
+}
+
+
+AudioRecorder.propTypes = {
+    sendAudio: PropTypes.func.isRequired,
+    nomConv: PropTypes.string.isRequired
 }
